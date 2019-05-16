dbc8e9843a90ee4db3175ea4d2a311dc8c966591
songtx <sdlvstx@163.com>
2019-05-17 00:00:15 +0800

    no message

---
+++ src/main/java/com/wss/templateproject/common/aop/LogAOP.java
@@ -0,0 +1,9 @@
+package com.wss.templateproject.common.aop;
+
+/**
+ * @创建人 songtx
+ * @创建时间 2019-05-16 22:40
+ * @描述
+ */
+public class LogAOP {
+}
---
+++ src/main/java/com/wss/templateproject/controller/PmsBrandController.java
@@ -0,0 +1,14 @@
+package com.wss.templateproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @创建人 songtx
+ * @创建时间 2019-05-16 20:43
+ * @描述
+ */
+@RestController
+@RequestMapping("/pmsBrand")
+public class PmsBrandController {
+}
